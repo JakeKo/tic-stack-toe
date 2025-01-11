8160adeff09c23d848519ff32babe3b577ffadbd
Jake Koperski <wijakoperski@gmail.com>
2025-01-11 15:32:07 -0600

    Created GameStats component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./app.css";
 import { playGame } from "./runner";
+import GameStats from "./gameStats";
 
 function App() {
+  const [playerAWins, setPlayerAWins] = useState(0);
+  const [playerBWins, setPlayerBWins] = useState(0);
+  const [draws, setDraws] = useState(0);
+
   useEffect(() => {
     const gameCount = 10;
-    const wins = [];
+    let playerAWinsTemp = playerAWins;
+    let playerBWinsTemp = playerBWins;
+    let drawsTemp = draws;
 
     for (let i = 0; i < gameCount; i++) {
-      wins.push(playGame());
+      const win = playGame();
+
+      if (win === "a") {
+        playerAWinsTemp++;
+      } else if (win === "b") {
+        playerBWinsTemp++;
+      } else {
+        drawsTemp++;
+      }
     }
 
-    console.log(wins);
+    setPlayerAWins(playerAWinsTemp);
+    setPlayerBWins(playerBWinsTemp);
+    setDraws(drawsTemp);
+    // eslint-disable-next-line
   }, []);
 
-  return <div className="app">Hello World!</div>;
+  return (
+    <div className="app">
+      Hello World!
+      <GameStats
+        playerAName={"A"}
+        playerBName={"B"}
+        playerAWins={playerAWins}
+        playerBWins={playerBWins}
+        draws={draws}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/gameStats.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import "./app.css";
+
+function GameStats({
+  playerAName,
+  playerBName,
+  playerAWins,
+  playerBWins,
+  draws,
+}) {
+  const [columns, setColumns] = useState("");
+
+  useEffect(() => {
+    const gameCount = playerAWins + playerBWins + draws;
+    setColumns(
+      `${playerAWins / gameCount}fr ${draws / gameCount}fr ${
+        playerBWins / gameCount
+      }fr`
+    );
+  }, [playerAWins, playerBWins, draws]);
+
+  return (
+    <div className="game-stats">
+      <div className="player-names">
+        <div>{playerAName}</div>
+        <div>{playerBName}</div>
+      </div>
+      <div className="win-stats" style={{ gridTemplateColumns: columns }}>
+        <div className="player-a-wins">{playerAWins}</div>
+        <div className="draws">{draws}</div>
+        <div className="player-b-wins">{playerBWins}</div>
+      </div>
+    </div>
+  );
+}
+
+export default GameStats;
