0c1fddaa8517781d9661cb4cd1d88c3fac4194ad
Jake Koperski <wijakoperski@gmail.com>
2025-04-18 08:37:24 -0500

    Improved visuals of BoardDisplay

--- src/boardDisplay.js
+++ src/boardDisplay.js
@@ -10,14 +10,22 @@ function BoardDisplay({ board, p1Name }) {
   function getCellColor(x, y) {
     const winner = getCellWinner(board.cells, [x, y]);
     if (winner) {
-      return winner === p1Name ? "green" : "red";
+      return winner === p1Name ? "lightgreen" : "lightcoral";
     } else {
       return "white";
     }
   }
 
+  function getPieceStyle(playerName, index) {
+    return {
+      borderColor: playerName === p1Name ? "green" : "red",
+      width: `${(index + 1) * 40}px`,
+      zIndex: board.slotCount - index,
+    };
+  }
+
   return (
-    <div className="board-display">
+    <div className="board-display" style={{ width: "500px" }}>
       {board.cells.map((col, x) =>
         col.map((cell, y) => (
           <div
@@ -28,7 +36,18 @@ function BoardDisplay({ board, p1Name }) {
               backgroundColor: getCellColor(x, y),
             }}
           >
-            {cell}
+            {cell.map((player, index) => {
+              if (player) {
+                return (
+                  <div
+                    key={`${x}-${y}-${index}`}
+                    className="piece"
+                    style={getPieceStyle(player, index)}
+                  ></div>
+                );
+              }
+              return null;
+            })}
           </div>
         ))
       )}
