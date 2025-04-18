19b89a744ae11b7427a217284f16190504993b17
Jake Koperski <wijakoperski@gmail.com>
2025-04-18 09:09:27 -0500

    Enabled scrolling through games

--- src/App.js
+++ src/App.js
@@ -33,6 +33,8 @@ function App() {
   const [playGames, setPlayGames] = useState(false);
   const p1Name = "P1";
   const p2Name = "P2";
+  const [games, setGames] = useState([]);
+  const [gameIndex, setGameIndex] = useState(-1);
 
   useEffect(() => {
     if (playGames) {
@@ -46,12 +48,11 @@ function App() {
 
   function playGameRecordResults() {
     const game = playGame(p1Name, p2Name);
-    const { board, winner } = game;
 
-    if (winner === p1Name) {
+    if (game.winner === p1Name) {
       recordP1GameResult("win", p2Name);
       recordP2GameResult("loss", p1Name);
-    } else if (winner === p2Name) {
+    } else if (game.winner === p2Name) {
       recordP1GameResult("loss", p2Name);
       recordP2GameResult("win", p1Name);
     } else {
@@ -59,8 +60,18 @@ function App() {
       recordP2GameResult("draw", p1Name);
     }
 
-    setBoard(board);
+    setGames((games) => [...games, game]);
   }
+
+  useEffect(() => {
+    setGameIndex(games.length - 1);
+  }, [games.length]);
+
+  useEffect(() => {
+    if (gameIndex >= 0) {
+      setBoard(games[gameIndex].board);
+    }
+  }, [gameIndex, games]);
 
   return (
     <div className="app">
@@ -71,8 +82,21 @@ function App() {
         p2Wins={p2History.wins}
         draws={p1History.draws}
       />
+      <button
+        onClick={() => setGameIndex(gameIndex - 1)}
+        disabled={gameIndex <= 0}
+      >
+        Previous Game
+      </button>
       <button onClick={playGameRecordResults}>Play Game</button>
       <button onClick={() => setPlayGames(!playGames)}>Play Games</button>
+      <button
+        onClick={() => setGameIndex(gameIndex + 1)}
+        disabled={gameIndex >= games.length - 1}
+      >
+        Next Game
+      </button>
+      {gameIndex} {games.length}
       <BoardDisplay board={board} p1Name={p1Name} p2Name={p2Name} />
     </div>
   );
