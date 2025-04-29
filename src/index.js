774985d862960bb6dc616f317c741f9da3b22dd5
Jake Koperski <wijakoperski@gmail.com>
2025-04-28 22:10:02 -0500

    Created separate pages for manual and auto playing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
+import AutoPlayer from "./pages/AutoPlayer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="/auto" element={<AutoPlayer />} />
+        <Route path="/manual" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
