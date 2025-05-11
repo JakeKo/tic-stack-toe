e23ee771c7da18d102459389fba28d3af9ac6e50
Jake Koperski <wijakoperski@gmail.com>
2025-05-10 22:46:26 -0500

    GamePiece now prevents dragging when inactive

--- src/components/GamePiece.js
+++ src/components/GamePiece.js
@@ -1,19 +1,22 @@
 import { useDrag } from "react-dnd";
 import { PIECE_SIZE_UNIT, gamePieceSize } from "../utils";
-import { useGamePieceColor } from "../store/game";
+import { useGame, useGamePieceColor } from "../store/game";
 
 function GamePiece({ playerName, size, address }) {
+  const game = useGame();
   const gamePieceColor = useGamePieceColor(playerName);
+  const canDrag = playerName === game.activePlayer?.name;
 
   const [, dragRef] = useDrag({
     type: "game-piece",
+    canDrag,
     item: { size, address, playerName },
   });
 
   return (
     <div
       ref={dragRef}
-      className="piece draggable"
+      className={`piece ${canDrag && "draggable"}`}
       style={{
         border: `${PIECE_SIZE_UNIT}px solid ${gamePieceColor}`,
         width: `${gamePieceSize(size)}px`,
