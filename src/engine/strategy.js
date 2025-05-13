a633aeae6f3d9ceddbe550327fb733dab1477226
Jake Koperski <wijakoperski@gmail.com>
2025-05-13 08:14:28 -0500

    Implemented weighted sampling method for random strategies

--- src/engine/strategy.js
+++ src/engine/strategy.js
@@ -1,3 +1,4 @@
+import { produce } from "immer";
 import { getAllOpenSlots, getAllPluckablePieces, getCellWinner } from "./board";
 
 function getAllPossibleCommands(cells, playerName, playerInventory) {
@@ -30,42 +31,58 @@ function getAllPossibleCommands(cells, playerName, playerInventory) {
   return allPossibleCommands;
 }
 
-function strategyRandom(player, game) {
-  const allPossibleCommands = getAllPossibleCommands(
-    game.board.cells,
-    player.name,
-    player.inventory
-  );
-
-  if (allPossibleCommands.length === 0) {
+function pickRandom(list, weights) {
+  if (list.length === 0) {
     return;
-  } else {
-    const randomIndex = Math.floor(Math.random() * allPossibleCommands.length);
-    return allPossibleCommands[randomIndex];
+  }
+
+  if (!weights) {
+    weights = Array(list.length).fill(1);
+  }
+
+  let runningWeight = 0;
+  const runningWeights = [];
+  for (let i = 0; i < weights.length; i++) {
+    runningWeight += weights[i];
+    runningWeights.push(runningWeight);
+  }
+
+  const randomPick = Math.random() * runningWeight;
+  for (let i = 0; i < runningWeights.length; i++) {
+    if (randomPick < runningWeights[i]) {
+      return list[i];
+    }
   }
 }
 
-function strategyRandomAvoidWonCells(player, game) {
-  const allPossibleCommands = getAllPossibleCommands(
-    game.board.cells,
-    player.name,
-    player.inventory
-  );
-  const commands = allPossibleCommands.filter((command) => {
-    const [x, y] = command.slot;
-    const winner = getCellWinner({ cells: game.board.cells, address: [x, y] });
-
-    return winner !== player.name;
+function devalueWonCells(weights, commands, cells, playerName) {
+  return produce(weights, (draftWeights) => {
+    commands.forEach((command, i) => {
+      const [x, y] = command.slot;
+      const winner = getCellWinner({ cell: cells[x][y] });
+      if (winner === playerName) {
+        draftWeights[i] *= 0.5;
+      }
+    });
   });
+}
 
-  if (commands.length === 0) {
-    return;
-  } else {
-    const randomIndex = Math.floor(Math.random() * commands.length);
-    const command = commands[randomIndex];
+function strategyRandom(player, game) {
+  const { cells } = game.board;
+  const { name, inventory } = player;
+  const commands = getAllPossibleCommands(cells, name, inventory);
 
-    return command;
-  }
+  return pickRandom(commands);
+}
+
+function strategyAvoidWonCells(player, game) {
+  const { cells } = game.board;
+  const { name, inventory } = player;
+  const commands = getAllPossibleCommands(cells, name, inventory);
+  let weights = Array(commands.length).fill(1);
+  weights = devalueWonCells(weights, commands, cells, name);
+
+  return pickRandom(commands, weights);
 }
 
 const STRATEGIES = {
@@ -76,7 +93,7 @@ const STRATEGIES = {
 const STRATEGY_FUNCS = {
   [STRATEGIES.MANUAL]: () => {},
   [STRATEGIES.RANDOM]: strategyRandom,
-  [STRATEGIES.RANDOM_AVOID_WON_CELLS]: strategyRandomAvoidWonCells,
+  [STRATEGIES.RANDOM_AVOID_WON_CELLS]: strategyAvoidWonCells,
 };
 
 export { getAllPossibleCommands, STRATEGY_FUNCS, STRATEGIES };
