2efc093890ca872b5aa3738f28739e0cf04c1c35
Jake Koperski <wijakoperski@gmail.com>
2025-05-17 17:12:31 -0500

    Revert to original BrowserHistory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Routes, Route, HashRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
 import AutoPlayer from "./pages/AutoPlayer";
 import ManualPlayer from "./pages/ManualPlayer";
@@ -15,13 +15,13 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <DndProvider backend={HTML5Backend}>
-        <HashRouter>
+        <BrowserRouter>
           <Routes>
             <Route index element={<App />} />
             <Route path="/auto" element={<AutoPlayer />} />
             <Route path="/manual" element={<ManualPlayer />} />
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
       </DndProvider>
     </Provider>
   </React.StrictMode>
